e0a56370b077b1eb0f3acc50b73bca13a9e44b35
coldmayo <wallachm@msu.edu>
2023-07-30 22:26:44 -0400

    felt cute, messed around with javascript ;P

---
+++ javascript/type.js
@@ -0,0 +1,43 @@
+// basically stolen from https://codepen.io/Coding_Journey/pen/BEMgbX
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["\xa0a Physics Major", "\xa0a Computer Scientist", "\xa0a Machine Learning Engineer", "\xa0a Life-long Learner"];
+const typingDelay = 120;   // how long it takes to type a letter
+const erasingDelay = 100;  // how long it takes to delete a letter
+const newTextDelay = 400;  // how long it takes in between words
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
