fddb413003c8afd3c8ee4b7cedb85965f1be2a9d
coldmayo <wallachm@msu.edu>
2023-10-03 16:15:22 -0400

    new blogpost + darkmode support

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,51 @@
+// checks browser's default mode (true if dark mode, false if light mode)
 const isDarkMode = () => {
     return window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+// changes the site theme to either dark or light mode and changes images accordingly
+function dark() {
+    var element = document.body;
+    var stat = element.classList.toggle("dark_mode");
+    if (stat == true) {
+        document.getElementById("icon").src = "imgs/sun.png";
+        if (document.getElementById("rust") !== null) {
+            document.getElementById("rust").src = "imgs/rust-modified.png";
+        } else if (document.getElementById("map") !== null) {
+            document.getElementById("map").src = "imgs/map-modified.png";
+            document.getElementById("phone").src = "imgs/phone-modified.png";   
+            document.getElementById("mail").src = "imgs/mail-modified.jpg";
+            document.getElementById("github").src = "imgs/github-modified.png";   
+        }
+        localStorage.setItem("mode", "true");
+    } else {
+        document.getElementById("icon").src = "imgs/moon.png";
+        if (document.getElementById("rust") !== null) {
+            document.getElementById("rust").src = "imgs/rust.png";
+        } else if (document.getElementById("map") !== null) {
+            document.getElementById("map").src = "imgs/map.png";
+            document.getElementById("phone").src = "imgs/phone.png"; 
+            document.getElementById("mail").src = "imgs/mail.jpg";
+            document.getElementById("github").src = "imgs/github.png";  
+        }
+        //document.getElementById("rust").src = "imgs/rust.png";
+        //document.getElementById("map").src = "imgs/map.png";
+        //document.getElementById("mail").src = "imgs/mail.png";
+        localStorage.setItem("mode", "false");
+    }
+}
+
+// on startup, checks weither or not site should be in dark or light mode and switches if needed
+window.onload = function() {
+    if("mode" in localStorage){
+        if (localStorage.getItem("mode") == "true") {
+            dark();
+        }
+    } else {
+        if (isDarkMode() == true) {
+            dark();
+        } else {
+            localStorage.setItem("mode", "true");
+        }
+    }
 }
