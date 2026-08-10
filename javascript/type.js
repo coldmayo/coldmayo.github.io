dd373255cabb7be930e628c7296cb1906769d97f
coldmayo <wallachm@msu.edu>
2026-08-09 23:15:28 -0400

    update expirence

--- javascript/type.js
+++ javascript/type.js
@@ -3,7 +3,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["\xa0a Physicist", "\xa0a Life-long Learner", "\xa0a Music Lover", "\xa0a Software Engineer", "\xa0a Low-Level Enthusiast"];
+const textArray = ["\xa0a Physicist", "\xa0a Music Lover", "\xa0a Software Engineer", "\xa0a Low-Level Enthusiast"];
 const typingDelay = 120;   // how long it takes to type a letter
 const erasingDelay = 100;  // how long it takes to delete a letter
 const newTextDelay = 250;  // how long it takes in between words
